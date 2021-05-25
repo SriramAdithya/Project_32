780c27cbe61bcb8735f3c0f3afb9991fcbab6456
Sriram Adithya <72451613+SriramAdithya@users.noreply.github.com>
2021-05-25 08:06:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const image = document.querySelector('#image');
+const animateBtn = document.querySelector('#btn__animate');
+let interval;
+
+// prettier-ignore
+const imageSource = src => image.src = src;
+
+async function getHour() {
+  try {
+    const request = await fetch('https://worldtimeapi.org/api/timezone/Asia/Kolkata');
+    const { datetime } = await request.json();
+    const hour = datetime.slice(11, 13);
+
+    if (hour <= 06) imageSource(`./images/sunrise2.png`);
+    else if (hour <= 10) imageSource(`./images/sunrise4.png`);
+    else if (hour <= 14) imageSource(`./images/sunrise5.png`);
+    else if (hour <= 17) imageSource(`./images/sunrise6.png`);
+    else if (hour <= 19) imageSource(`./images/sunrise7.png`);
+    else if (hour <= 21) imageSource(`./images/sunrise9.png`);
+    else if (hour === 22) imageSource(`./images/sunrise10.png`);
+    else imageSource(`./images/sunrise11.png`);
+    /* ------------------------------------------------------------- */
+  } catch {
+    setTimeout(function () {
+      getHour();
+    }, 2000);
+  }
+}
+
+getHour();
+
+function animate() {
+  let currImg = 0;
+  if (interval) clearInterval(interval);
+  interval = setInterval(function () {
+    currImg !== 11 ? imageSource(`./images/sunrise${++currImg}.png`) : clearInterval(interval);
+  }, 1000);
+}
+
+animateBtn.addEventListener('click', animate);
